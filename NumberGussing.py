75ad6979c3d1537cd151c48bd7fb06df8185a3f6
n2-singh <nishasingh4449267@gmail.com>
2026-02-22 19:13:01 +0530

    Add files via upload

---
+++ NumberGussing.py
@@ -0,0 +1,16 @@
+import random
+number=random.randint(1,100)
+attempts=0
+print("Welcome to the Number Gussing Game!")
+print("I have selected a number between 1 and 100.")
+while True:
+  guess=int(input("Enter your guess:"))
+  attempts+=1
+  if guess<number:
+    print("Too Low! Try again.")
+  elif guess>number:
+    print("Too High! Try again.")
+  else:
+    print("Congratulations! You guessed the number.")
+    print("Total attempts:",attempts)
+    break
